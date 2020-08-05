a95bf5b961b19599798d659ddaf7c1166ca40530
EstebanGameDevelopment <services@yourvrexperience.com>
2020-08-05 20:50:04 +0200

    When WebGL configuration enabled the VR gaze should be disabled.

--- scripts/controllers/EventSystemController.cs
+++ scripts/controllers/EventSystemController.cs
@@ -46,7 +46,7 @@ namespace YourVRUI
 		// PRIVATE MEMBERS
 		// ----------------------------------------------	
 		private StandaloneInputModule m_standAloneInputModule;
-#if !ENABLE_OCULUS && UNITY_HAS_GOOGLEVR
+#if (!ENABLE_OCULUS && UNITY_HAS_GOOGLEVR) || UNITY_WEBGL
 		private GvrPointerInputModule m_gazeInputModule;
 #endif
 
@@ -77,7 +77,7 @@ namespace YourVRUI
 				this.gameObject.AddComponent<StandaloneInputModule>();
 				m_standAloneInputModule = this.gameObject.GetComponent<StandaloneInputModule>();
 			}
-#if !ENABLE_OCULUS && UNITY_HAS_GOOGLEVR
+#if (!ENABLE_OCULUS && UNITY_HAS_GOOGLEVR) || UNITY_WEBGL
 			m_gazeInputModule = this.gameObject.GetComponent<GvrPointerInputModule>();
 			if (m_gazeInputModule == null)
 			{
@@ -121,8 +121,10 @@ namespace YourVRUI
 				if (m_standAloneInputModule != null) m_standAloneInputModule.enabled = activation;
 #if ENABLE_WORLDSENSE
                 if (m_gazeInputModule != null) m_gazeInputModule.enabled = false;
-#elif !ENABLE_OCULUS && UNITY_HAS_GOOGLEVR
+#elif !ENABLE_OCULUS && UNITY_HAS_GOOGLEVR && !UNITY_WEBGL
                 if (m_gazeInputModule != null) m_gazeInputModule.enabled = !activation;
+#elif UNITY_WEBGL
+                if (m_gazeInputModule != null) m_gazeInputModule.enabled = false;
 #endif
             }
             if (_nameEvent == EVENT_EVENTSYSTEMCONTROLLER_RAYCASTING_SYSTEM)
